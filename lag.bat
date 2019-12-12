cd0bc6b94c8f8c07282d47cc769a21202e5c1d3f
Leobastiani <leogbastiani@gmail.com>
2019-12-12 20:24:54 -0300

    Updated lag.bat to LeagueClient.exe

--- lag.bat
+++ lag.bat
@@ -1,11 +1,15 @@
 @ECHO OFF
 if NOT EXIST %~dp0\lag.txt (
 	echo.>%~dp0\lag.txt
-	call hide me
-	call netlimiter start
-	nircmdc waitprocess "League of Legends.exe"
-	call netlimiter stop
+	if ["%~1"] == [""] (
+		call hide me
+	)
+	call netlimiter start %*
+	nircmdc waitprocess "LeagueClient.exe"
+	call netlimiter stop %*
 	del %~dp0\lag.txt
-	call show me
+	if ["%~1"] == [""] (
+		call show me
+	)
 	exit
 )
