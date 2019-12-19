192c6741e33c6cb9131114855d2f54e4e3936f47
Leobastiani <leogbastiani@gmail.com>
2019-12-19 01:14:46 -0300

    Added rerun

--- lag.bat
+++ lag.bat
@@ -1,10 +1,23 @@
 @ECHO OFF
+SETLOCAL EnableDelayedExpansion
+
+if EXIST %~dp0\lag.txt (
+	set /p ans=lag.bat is already been running, should it run again? [y]:
+	if ["!ans!"] == ["y"] (
+		del %~dp0\lag.txt
+	)
+)
+
 if NOT EXIST %~dp0\lag.txt (
 	echo.>%~dp0\lag.txt
+	call netlimiter start %*
+	if ["%ERRORLEVEL%"] NEQ ["0"] (
+		pause
+		goto:eof
+	)
 	if ["%~1"] == [""] (
 		call hide me
 	)
-	call netlimiter start %*
 	nircmdc waitprocess "LeagueClient.exe"
 	call netlimiter stop %*
 	del %~dp0\lag.txt
